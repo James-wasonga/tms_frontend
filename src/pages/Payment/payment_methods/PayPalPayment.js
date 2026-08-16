e71ca13a990ad87136c8874c5d0cdee163997129
James-wasonga <jameswasonga22@gmail.com>
2026-08-16 23:56:00 +0300

    clean code, PayPalPayment.js file

--- src/pages/Payment/payment_methods/PayPalPayment.js
+++ src/pages/Payment/payment_methods/PayPalPayment.js
@@ -1,18 +1,3 @@
-// import React from 'react'
-// import { useNavigate } from 'react-router-dom';
-
-// const PayPalPayment = () => {
-//   const navigate = useNavigate();
-//   const handlePay = ()=>{
-//     navigate("/profile");
-//   }
-//   return (
-//     <div>PayPalPayment</div>
-//   )
-// }
-
-// export default PayPalPayment;
-
 import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { CartContext, UserContext } from '../../../contexts';
